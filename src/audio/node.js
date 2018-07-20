bfc4a0310bf7c7a5135e66e91678818fe1083a40
anematode <timothy.herchen@gmail.com>
2018-07-19 21:04:35 -0700

    improve Node, SourceNode, EndingNode
    Signed-off-by: anematode <timothy.herchen@gmail.com>

--- src/audio/node.js
+++ src/audio/node.js
@@ -7,9 +7,21 @@ class Node {
     constructor() {
         this.entry = audio.Context.createGain(); // entry to node
         this.exit = audio.Context.createGain(); // exit to node
+        this.destroyed = false;
+    }
+
+    _checkDestroyed() {
+        if (this.destroyed)
+            throw new Error("Node is destroyed");
+    }
+
+    isDestroyed() {
+        return this.destroyed;
     }
 
     connect(x) { // connect to node
+        this._checkDestroyed();
+
         if (x instanceof Node) {
             this.exit.connect(x.entry);
         } else if (x instanceof EndingNode) {
@@ -21,15 +33,32 @@ class Node {
     }
 
     disconnect() { // disconnect from node
+        this._checkDestroyed();
+
         this.exit.disconnect();
         return this;
     }
 
-    destroy() { // destroy the node TODO
+    destroy() {
+        this._checkDestroyed();
+
+        try {
+            this.stop();
+        } catch (e) {}
+
         this.disconnect();
+        this.connect(audio.voidNode); // allow further render quantums so the stop call propagates
+
+        setTimeout(() => {
+            this.exit.disconnect();
+        }, 1000); // Disconnect it later
+
+        this.destroyed = true;
     }
 
     connectToMaster() { // connect the node to master
+        this._checkDestroyed();
+
         this.connect(audio.masterEntryNode);
     }
 }
@@ -40,9 +69,21 @@ Node with no input producing audio
 class SourceNode {
     constructor() {
         this.exit = audio.Context.createGain();
+        this.destroyed = false;
+    }
+
+    _checkDestroyed() {
+        if (this.destroyed)
+            throw new Error("Node is destroyed");
+    }
+
+    isDestroyed() {
+        return this.destroyed;
     }
 
     connect(x) { // connect to node
+        this._checkDestroyed();
+
         if (x instanceof Node) {
             this.exit.connect(x.entry);
         } else if (x instanceof EndingNode) {
@@ -53,15 +94,37 @@ class SourceNode {
         return this;
     }
 
-    disconnect() { // disconnect from node, on timeout to allow remaining render quantums
-        setTimeout(() => {
+    disconnect(x) { // disconnect from node
+        this._checkDestroyed();
+
+        if (!x) {
             this.exit.disconnect();
-        }, 50);
+        } else if (x instanceof Node) {
+            this.exit.disconnect(x.entry);
+        } else if (x instanceof EndingNode) {
+            this.exit.disconnect(x.entry);
+        } else {
+            this.exit.disconnect(x);
+        }
+
         return this;
     }
 
     destroy() { // destroy the node
+        this._checkDestroyed();
+
+        try {
+            this.stop();
+        } catch (e) {}
+
         this.disconnect();
+        this.connect(audio.voidNode); // allow further render quantums so the stop call propagates
+
+        setTimeout(() => {
+            this.exit.disconnect();
+        }, 1000); // Disconnect it later
+
+        this.destroyed = true;
     }
 
     connectToMaster() { // connect to master
@@ -75,14 +138,20 @@ node with no output
 class EndingNode {
     constructor() {
         this.entry = audio.Context.createGain();
+        this.source = null;
     }
 
-    connectFrom(c) { // connect from input node
-        c.connect(this.entry);
+    connectFrom(node) { // connect from input node
+        node.connect(this.entry);
+        this.source = node;
     }
     
     connectFromMaster() { // connect from master node
         this.connectFrom(audio.masterEntryNode);
+    }
+
+    disconnectFrom() { // disconnect from node
+        this.source.disconnect(this.entry);
     }
 }
 
